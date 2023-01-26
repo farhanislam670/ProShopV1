954a150652297a1ea277f672ed33551a80f073d4
Mohammed Farhan <farhanislam670@gmail.com>
2023-01-26 15:25:16 +0600

    JWT authentication fails due to token not being passed into authMiddleware.js

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.header.authorization &&
+    req.header.authorization.startsWith("Bearer")
+  ) {
+    try {
+      //   The line below extracts the token from the headers into
+      //   "Bearer" $ "token" which is stores in index 1 of the array
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      console.log(decoded);
+
+      next();
+    } catch (error) {}
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+export { protect };
