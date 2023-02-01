804fb6d96736fc102c3ba9cb11c692ea00866c23
Mohammed Farhan <farhanislam670@gmail.com>
2023-02-01 13:55:31 +0600

    User registration done.

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -4,8 +4,6 @@ import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
-
-  console.log(req.headers.authorization);
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
@@ -17,10 +15,14 @@ const protect = asyncHandler(async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      console.log(decoded);
+      req.user = await User.findById(decoded.id).select("-password");
 
       next();
-    } catch (error) {}
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
   }
 
   if (!token) {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 const router = express.Router();
-import { authUser, getUserProfile } from "../controllers/userController.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
+router.route("/").post(registerUser); //This format is my default POST route.
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+
+router.route("/profile").get(protect, getUserProfile); //This format is my default GET route.
 
 export default router;
